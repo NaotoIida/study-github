08863017be4ed94437ce599a58bc666fc7aa95cf
N.I <8beim006@mail.u-tokai.ac.jp>
2019-10-02 16:03:17 +0900

    done running ttov.sh.

--- ind_words_fil.rb
+++ ind_words_fil.rb
@@ -11,22 +11,30 @@ inp = File.open(ARGV[0],"r")
 M = Array.new
 
 #Substitute indipendent words to matrix M.
+puts("m1")
 inp.each do |i|
    m1 = i.chomp!.split(",")
    m2 = m1[0].split("\t")
 
    if (m2[1]=='動詞' || m2[1]=='形容詞' || m2[1]=='名詞' || m2[1]=='副詞' || m2[1]=='接続詞' || m2[1]=='感動詞' || m2[0]=='EOS')
-      M.push(m1)
+      puts("#{i}\n")
+      M.push(i)
    end
 end
 
-p M
+puts M
 
 #Output matrix M.
 outp = File.open(ARGV[1],"w")
+puts M
 M.each do |i|
-   outp.printf("%s",i)
+#   outp.printf("%s",i)
+#   outp.printf("\n")
+#   puts i
+#   puts("\n")
 end
+
+outp.puts(M)
 
 inp.close
 outp.close
